eb37324d763e5f3cf41242adeedcd4f322d0295a
Miguel Higuera <mf.higuera@uniandes.edu.co>
2025-11-01 20:00:34 -0500

    Punto 2

--- src/app/vehiculo/vehiculo-list/vehiculo-list.component.ts
+++ src/app/vehiculo/vehiculo-list/vehiculo-list.component.ts
@@ -10,13 +10,19 @@ import { VehiculoService } from '../vehiculo.service';
 export class VehiculoListComponent implements OnInit {
 
   vehiculos: Array<Vehiculo> = [];
+  totalesPorMarca: { [marca: string]: number } = {};
 
   constructor(private vehiculoService: VehiculoService) { }
 
   getVehiculos(): void {
     this.vehiculoService.getVehiculos().subscribe(vehiculos => {
       this.vehiculos = vehiculos;
+      this.calcularTotales();
     });
+  }
+
+  calcularTotales(): void {
+    this.totalesPorMarca = this.vehiculos.reduce((acc: { [key: string]: number }, v) => ((acc[v.marca] = (acc[v.marca] || 0) + 1), acc), {});
   }
 
   ngOnInit() {
